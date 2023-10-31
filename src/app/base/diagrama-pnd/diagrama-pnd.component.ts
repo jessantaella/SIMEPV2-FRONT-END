05f873c05840765f694b89c502e14c9a0fda48b5
jessantaella <jesus.santaella@b-process.mx>
2023-10-30 20:44:07 -0600

    modificación
    Diagrama pnd con svg

--- src/app/base/diagrama-pnd/diagrama-pnd.component.ts
+++ src/app/base/diagrama-pnd/diagrama-pnd.component.ts
@@ -21,8 +21,32 @@ export class DiagramaPndComponent {
   interval:any;
   mostrarMovil:boolean = false;
   //nd:true,desarrollo:true,fondos:true
-  ocultarSecciones = [false,false,false];
+  //ocultarSecciones = [false,false,false];
 
+  mostrarPnd=false;
+  mostrarFederales=false;
+  mostrarRamo=false;
+
+
+  tamanioSecciones = [
+  {height:'60.26',heightOriginal:'234.26'},
+  {height:'50.26',heightOriginal:'125'},
+  {height:'40.26',heightOriginal:'125'}
+  ];
+
+  separadoDiagrama = [
+   { pndOriginal:{
+      fondo: '378.29',
+      texto: '379.75',
+      conector:'0'
+    },
+  pndNuevo:{
+    fondo : '210',
+    texto : '210',
+    conector : '-170' 
+  }
+  }
+  ]
 
   public innerWidth: any;
   constructor( private breakpointObserver: BreakpointObserver,private base:ServicesBaseService) {
@@ -67,7 +91,43 @@ export class DiagramaPndComponent {
     console.log("rAMO"); 
   }
 
-  mostrarSeccion(opcion:number){
-    this.ocultarSecciones[opcion]=!this.ocultarSecciones[opcion];
+  mostrarEvent(tipo:number){
+    if(tipo===1){
+      this.mostrarPnd = !this.mostrarPnd;
+    }
+    else if(tipo ===2){
+      this.mostrarFederales = !this.mostrarFederales;
+    }
+    else if(tipo===3){
+      this.mostrarRamo = !this.mostrarRamo;
+    }
+    console.log(this.mostrarPnd);
+  }
+
+  obtenerTamanio(tipo:number){
+    let salida='100';
+    switch( tipo){
+      case 1:
+        salida =  this.mostrarPnd ? this.tamanioSecciones[0].heightOriginal : this.tamanioSecciones[0].height;
+      break;
+      case 2:
+        salida =  this.mostrarFederales ? this.tamanioSecciones[1].heightOriginal : this.tamanioSecciones[1].height;
+      break;
+      case 3: 
+        salida =  this.mostrarRamo ? this.tamanioSecciones[2].heightOriginal : this.tamanioSecciones[2].height;
+       break;
+    }
+    return salida;
+  }
+
+  obtenerPosicion(tipo:number){
+    let salida={fondo:'',texto:'',conector:''};
+    switch( tipo){
+      case 1:
+        salida =  !this.mostrarPnd ? this.separadoDiagrama[0].pndNuevo : this.separadoDiagrama[0].pndOriginal;
+      break;
+    }
+    console.log(salida)
+    return salida;
   }
 }
