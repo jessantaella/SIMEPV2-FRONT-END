4ba34e4607c8feccebd969a09857b6890535aab1
Gibran Ojeda <gibran.ojeda.7@gmail.com>
2023-11-01 15:03:23 -0600

    Revert "Merge branch 'main' into dev/gibs"
    This reverts commit 0a18252318e62b13d15f20a3e99e90952540051c, reversing
    changes made to 60e12cfb83705775e1d3a97059a6baffca93b357.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
-import { DataDynamic } from './base/services/dinamic-data.services';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +6,5 @@ import { DataDynamic } from './base/services/dinamic-data.services';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SIMEPS';
-  tags:any;
-
-  constructor(private meta: Meta,private servicio:DataDynamic){
-    this.consultarTags();
-   }
-
-  consultarTags(){
-    this.servicio.getInformacion().subscribe(
-      res=>{
-        this.tags = res.simeps?.metas;
-        this.addTags();
-      }
-    )
-   }
-
-   addTags(){
-    this.tags.forEach((tg: { name: any; content: any; }) => {
-      this.meta.addTag({ name: tg.name , content: tg.content});
-      console.log('add Meta')
-    });
-   }
+  title = 'simepsv2-front-end';
 }
