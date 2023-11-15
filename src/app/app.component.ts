df2772051e6392e9f597b1ba22cda9689a228818
jessantaella <jesus.santaella@b-process.mx>
2023-11-15 16:23:53 -0600

    #add
    Correcciones y validación de GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
 import { DataDynamic } from './base/services/dinamic-data.services';
+import { get } from "scriptjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'SIMEPS';
   version = 'V-1.0.0'+ new Date(); ;
   tags:any;
+  ga:any;
 
   constructor(private meta: Meta,private servicio:DataDynamic){
     this.consultarTags();
     console.log(this.version)
    }
 
+   cargaGA() {
+    try{
+      console.log(this.ga)
+      get(this.ga, () => {
+        //Google Maps library has been loaded...
+        console.log("cargo js")
+    });
+    }catch(io){
+      console.log('Error al cargar js de GA')
+    }
+  
+  }
+
   consultarTags(){
     this.servicio.getInformacion().subscribe(
       res=>{
         this.tags = res.simeps?.metas;
+        this.ga = res.simeps?.ga?.url;
+        this.cargaGA();
         this.addTags();
       }
     )
