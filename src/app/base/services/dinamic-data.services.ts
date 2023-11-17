b1fea0e387e608caceb494d073647b541b038074
jessantaella <jesus.santaella@b-process.mx>
2023-11-16 19:08:20 -0600

    #ADD
    SSR funcionando

--- src/app/base/services/dinamic-data.services.ts
+++ src/app/base/services/dinamic-data.services.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +8,8 @@ export class DataDynamic {
 constructor(private http:HttpClient){}
 
   getInformacion(){
-    const url:string = "http://127.0.0.1:5500/dist/configuracion.json";
-    //const url:string = "http://10.1.15.180/Configuracion/configuracion.json";
+    //const url:string = "http://127.0.0.1:5500/dist/configuracion.json";
+    const url:string = "http://10.1.15.180/Configuracion/configuracion.json";
     return this.http.get<any>(url);
   }
 }
