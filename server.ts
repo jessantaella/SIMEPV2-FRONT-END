5068466bd1304bab306203fae427c6f3138db2b4
JoaquinMartinezBprocess <Joaquin.martinez@b-process.mx>
2023-12-12 14:32:52 -0600

    Correcciones sonar

--- server.ts
+++ server.ts
@@ -10,8 +10,8 @@ import { AppServerModule } from './src/main.server';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  //const distFolder = join(process.cwd(), '/SIMEPS/browser');  //Despliegue
-  const distFolder = join(process.cwd(), 'dist/simeps/browser'); //LOCAL
+  const distFolder = join(process.cwd(), '/SIMEPS/browser');  //Despliegue
+  //const distFolder = join(process.cwd(), 'dist/simeps/browser'); //LOCAL
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
