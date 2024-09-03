300b95ebf319ad7eaf01a68dd81357589ae81477
CarlosRamosBP <carlos.ramos@b-process.mx>
2024-09-03 12:43:50 -0600

    Codigo base del modulo Programas-indicadores1924

--- src/app/base/programas-indicadores1924/programas-indicadores1924.component.ts
+++ src/app/base/programas-indicadores1924/programas-indicadores1924.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser, ViewportScroller } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, HostListener, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { DataDynamic } from '../services/dinamic-data.services';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { WINDOW } from '../services/window.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-programas-indicadores1924',
   templateUrl: './programas-indicadores1924.component.html',
   styleUrls: ['./programas-indicadores1924.component.scss']
 })
-export class ProgramasIndicadores1924Component {
+export class ProgramasIndicadores1924Component implements OnInit, AfterViewInit{
+  nivelSeleccionado: any;
+  menuSeleccionado = 1;
+  redes: any;
+  nombreSistema: any;
+  auxMascara = false;
+  fontSizeTitulo = '24px';
+  fontSizeTituloNormal = '20px';
+  isBrowser = false;
+  celular = false;
+  @ViewChild('planeacion')
+  planeacion!: ElementRef;
+  alto=100;
+  esMovil = false;
+  esTablet = false;
+  esEscritorio = false;
 
+  plantilla = '';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(WINDOW) private window: Window,
+    @Inject(PLATFORM_ID) private platformId: any,
+    private scroller: ViewportScroller,
+    private servicio: DataDynamic,
+    private router: Router,
+    
+    private breakpointObserver: BreakpointObserver,
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    this.consultarData();
+    if (this.isBrowser) {
+      this.breakpointObserver
+        .observe(['(max-width: 576px)', '(min-width: 577px) and (max-width: 1200px)', '(min-width: 1201px)'])
+        .subscribe((result: BreakpointState) => {
+          if (result.breakpoints['(max-width: 576px)']) {
+            this.esMovil = true;
+            this.esTablet = false;
+            this.esEscritorio = false;
+          } else if (result.breakpoints['(min-width: 577px) and (max-width: 1200px)']) {
+            this.esMovil = false;
+            this.esTablet = true;
+            this.esEscritorio = false;
+          } else if (result.breakpoints['(min-width: 1201px)']) {
+            // Code for resolution greater than or equal to 993px
+            this.esMovil = false;
+            this.esTablet = false;
+            this.esEscritorio = true;
+          }
+        });
+    }
+  }
+  ngAfterViewInit(): void {
+    if (this.isBrowser) {
+      setInterval(() => {
+        let aux = this.planeacion?.nativeElement.offsetHeight;
+        this.alto = aux + aux / 6;
+      }, 10);
+    }
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      document.body.scrollTop = 0;
+      let pos = this.scroller.getScrollPosition();
+      if (pos[1] > 0) {
+        this.scroller.scrollToPosition([0, 0]);
+      }
+    }
+  }
+  consultarData() {
+    if (this.isBrowser) {
+      this.servicio.getInformacion().subscribe((res) => {
+        console.log(res);
+        this.nombreSistema = res?.simeps?.opciones[1].titulo;
+        this.redes = res.generales.redes;
+      });
+    }
+  }
+
+  
 }
