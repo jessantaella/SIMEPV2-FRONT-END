989b8009cd035121b6adf341a234dca9f600d20b
CarlosRamosBP <carlos.ramos@b-process.mx>
2024-09-04 22:09:48 -0600

    Agregada animacion a menu

--- src/app/base/indicador-sectorial1924/detalles-ind1924/detalles-ind1924.component.ts
+++ src/app/base/indicador-sectorial1924/detalles-ind1924/detalles-ind1924.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ObjetivoSectorial } from '../../Models/ObjetivoSectorial';
 import { IndicadorSectorial } from '../../Models/IndicadorSectorial';
 
@@ -8,6 +8,7 @@ import { IndicadorSectorial } from '../../Models/IndicadorSectorial';
   styleUrls: ['./detalles-ind1924.component.scss']
 })
 export class DetallesInd1924Component {
+  @ViewChild('menuDesplegable') menuDesplegable!: ElementRef;
   listaObjetivosSectoriales4T: ObjetivoSectorial[] = [
     {
         NUM_OBJETIVO: 1,
@@ -49,5 +50,41 @@ export class DetallesInd1924Component {
         ],
     }
   ];
+  
+
+  mostrarOcultar() {
+    const menuElement = this.menuDesplegable.nativeElement;
+    const screenWidth = window.innerWidth; // Obtén el ancho de la pantalla
+  
+    if (menuElement.classList.contains('menuVisible')) {
+      menuElement.classList.remove('menuVisible');
+      
+      if (screenWidth > 992) { 
+        menuElement.style.maxWidth = "0";
+        
+      }else{
+        menuElement.style.maxHeight = "0";
+      }
+      
+      setTimeout(() =>{
+        menuElement.style.display = "none";
+      }, 300)
+      menuElement.style.opacity = "0";
+    } else {
+      menuElement.style.display = "block";
+      menuElement.classList.add('menuVisible');
+      setTimeout(() =>{
+        if (screenWidth > 992) { // Ajusta el tamaño según tu necesidad
+          menuElement.style.maxWidth = "10000px";
+          menuElement.style.maxHeight = "none";
+        } else {
+          menuElement.style.maxWidth = "none";
+          menuElement.style.maxHeight = "10000px"; // Ajusta el tamaño según tu necesidad
+        }
+        menuElement.style.opacity = "1";
+        menuElement.style.display = "block";
+      }, 100)
+    }
+  }
 
 }
