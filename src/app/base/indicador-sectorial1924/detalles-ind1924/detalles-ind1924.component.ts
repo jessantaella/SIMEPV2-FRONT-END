f37445c4275ff42d4d2ad87b7625cdf11e1f1d6a
CarlosRamosBP <carlos.ramos@b-process.mx>
2024-09-13 13:49:46 -0600

    Pasado ejemplo a componente grafica

--- src/app/base/indicador-sectorial1924/detalles-ind1924/detalles-ind1924.component.ts
+++ src/app/base/indicador-sectorial1924/detalles-ind1924/detalles-ind1924.component.ts
@@ -3,7 +3,7 @@ import { ObjetivoSectorial } from '../../Models/ObjetivoSectorial';
 import { IndicadorSectorial } from '../../Models/IndicadorSectorial';
 import { DerechoSocialInd } from '../../Models/DerechoSocialInd';
 import { ActivatedRoute } from '@angular/router';
-import {Chart, registerables } from 'chart.js';
+
 
 @Component({
   selector: 'app-detalles-ind1924',
@@ -12,7 +12,6 @@ import {Chart, registerables } from 'chart.js';
 })
 export class DetallesInd1924Component {
   @ViewChild('menuDesplegable') menuDesplegable!: ElementRef;
-  chart: any;
   idProgramaSector: string | null = null;
   screenWidth: number;
   listaDerechosSocialesAsociados: DerechoSocialInd[] = [
@@ -73,15 +72,11 @@ export class DetallesInd1924Component {
   ngAfterViewInit(): void {
     this.screenWidth = window.innerWidth;
     this.changeCollapseMode(this.screenWidth);
-    setTimeout(() =>{
-      this.crearGrafica(); 
-    })
   }
   
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.idProgramaSector = params['idProgramaSect'];
-      console.log({program: params['idProgramaSect']});
     }); 
     
 
@@ -105,93 +100,7 @@ export class DetallesInd1924Component {
 
   }
 
-  crearGrafica() {
-    Chart.register(...registerables);
-    this.chart = new Chart('divGrafIndicadorSectorial', {
-      type: 'line', // Tipo de gráfico: línea
-      data: {
-        labels: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024'], // Años
-        datasets: [
-          {
-            label: 'Línea Base',
-            data: [null, null, null, null, null, 75,null,null,null,null,null,null], // Un solo punto para Línea Base
-            borderColor: 'gray',
-            backgroundColor: 'gray',
-            borderDash: [5, 5],
-            tension: 0.4,
-            fill: false,
-            pointStyle: 'circle',
-            pointRadius: 7,
-            pointHoverRadius: 7
-          },
-          {
-            label: 'Meta Planteada',
-            data: [null, null, null, null, null, null, null,null, null, null, null, 80], // Datos de Meta Intermedia
-            borderColor: 'gray',
-            backgroundColor: 'gray',
-            tension: 0.4,
-            fill: false,
-            pointStyle: 'circle',
-            pointRadius: 5,
-            pointHoverRadius: 7
-          },
-          {
-            label: 'Meta Alcanzada',
-            data: [81.5, 80.3, 79, 78.3, 76.9, 75, 73.7, 73.5, 70.8, 69.5, null, null], // Datos de Meta Alcanzada
-            borderColor: 'green',
-            backgroundColor: 'green',
-            tension: 0.4, // Suavizado de las líneas
-            fill: false,
-            pointStyle: 'circle',
-            pointRadius: 5,
-            pointHoverRadius: 7
-          },
-          {
-            label: 'Meta Intermedia',
-            data: [null, null, null, null, null, null, null,76.7, 77.5, 78.3, 79.2, 80], // Datos de Meta Intermedia
-            borderColor: 'blue',
-            backgroundColor: 'blue',
-            tension: 0.4,
-            fill: false,
-            pointStyle: 'circle',
-            pointRadius: 5,
-            pointHoverRadius: 7
-          },
-        ]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            display: true,
-            position: 'bottom'
-          },
-          title: {
-            display: true,
-            text: '1.1 Coeficiente de Autosuficiencia Alimentaria.',
-            font: {
-              size: 16
-            }
-          },
-          tooltip: {
-            enabled: true,
-            mode: 'index',
-            intersect: false,
-          }
-        },
-        scales: {
-          y: {
-            beginAtZero: false,
-            min: 65, // El mínimo del eje Y es 65
-            max: 85, // El máximo del eje Y es 85
-            ticks: {
-              stepSize: 5 // Incrementos de 5 unidades
-            }
-          }
-        }
-      }
-    });
-  }
+  
 
   
   
--- src/app/base/shared/grafica/grafica.component.ts
+++ src/app/base/shared/grafica/grafica.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-grafica',
@@ -6,5 +7,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./grafica.component.scss']
 })
 export class GraficaComponent {
+
+  chart: any;
+
+  ngAfterViewInit(): void {
+    setTimeout(() =>{
+      this.crearGrafica(); 
+    })
+    
+  }
+
+
+  crearGrafica() {
+    Chart.register(...registerables);
+    this.chart = new Chart('divGrafIndicadorSectorial', {
+      type: 'line', // Tipo de gráfico: línea
+      data: {
+        labels: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024'], // Años
+        datasets: [
+          {
+            label: 'Línea Base',
+            data: [null, null, null, null, null, 75,null,null,null,null,null,null], 
+            borderColor: 'gray',
+            backgroundColor: 'gray',
+            pointRadius: 4,
+          },
+          {
+            label: 'Meta Planteada',
+            data: [null, null, null, null, null, null, null,null, null, null, null, 80],
+            borderColor: 'gray',
+            backgroundColor: 'gray',
+            pointRadius: 4,
+          },
+          {
+            label: 'Meta Alcanzada',
+            data: [81.5, 80.3, 79, 78.3, 76.9, 75, 73.7, 73.5, 70.8, 69.5, null, null], 
+            borderColor: 'green',
+            backgroundColor: 'green',
+            pointRadius: 4,
+          },
+          {
+            label: 'Meta Intermedia',
+            data: [null, null, null, null, null, null, null,76.7, 77.5, 78.3, 79.2, 80], 
+            borderColor: 'blue',
+            backgroundColor: 'blue',
+            pointRadius: 4,
+          },
+        ]
+      },
+      options: {
+        responsive: true,
+        animation: {
+          duration: 2000 // Ajusta la duración en milisegundos (2 segundos en este caso)
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom'
+          },
+          title: {
+            display: true,
+            text: '1.1 Coeficiente de Autosuficiencia Alimentaria.',
+            font: {
+              size: 16
+            }
+          },
+          tooltip: {
+            enabled: true,
+            mode: 'index',
+            intersect: false,
+          }
+        },
+        scales: {
+          y: {
+            beginAtZero: false,
+            min: 65, // El mínimo del eje Y es 65
+            max: 85, // El máximo del eje Y es 85
+            ticks: {
+              stepSize: 5 // Incrementos de 5 unidades
+            }
+          }
+        }
+      }
+    });
+  }
 
 }
