75e76b766dfd709f681ce55ca68e26ecac1bcb42
ChristopherRAP <168852956+ChristopherRAP@users.noreply.github.com>
2024-11-11 19:03:32 -0600

    correccion sonar

--- src/app/base/indice201312018/indice201312018.component.ts
+++ src/app/base/indice201312018/indice201312018.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-indice201312018',
   templateUrl: './indice201312018.component.html',
-  styleUrls: ['./indice201312018.component.scss']
+  //styleUrls: ['./indice201312018.component.scss']
 })
 export class Indice201312018Component {
 
