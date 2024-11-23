70b5235d170ad324113af87ea6ba280815d6804c
ChristopherRAP <168852956+ChristopherRAP@users.noreply.github.com>
2024-11-23 00:16:26 -0600

    url dinamico services PND

--- src/app/plan-nacional/service/pnd2013.service.ts
+++ src/app/plan-nacional/service/pnd2013.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ServerConfService } from 'src/app/server-confing.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,14 @@ export class Pnd2013Service {
 
 
 
-  servidor= 'http://devnet.coneval.org.mx:93/MS-SIMEPS/api';
+  //http://10.1.15.101:8000/api/MS-SIMEPS/api/PND1318/MetasNacionales
 
-  constructor(private http:HttpClient) {
+  //servidor= 'http://devnet.coneval.org.mx:93/MS-SIMEPS/api';
+servidor='';
+
+  constructor(private http:HttpClient, private serverConfigService: ServerConfService)
+   {
+    this.servidor = this.serverConfigService.getServerConfig()+'api/MS-SIMEPS/api';
    }
 
    getConsultaMetasNacionales():Observable<any>{
